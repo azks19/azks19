312e85f4870cfb2138ca7b84e0f6d1e8c271c9e0
Aurang zaib <azks1993@gmail.com>
2021-05-26 21:34:58 +0500

    Sign up back end

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect database
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Api running"));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
